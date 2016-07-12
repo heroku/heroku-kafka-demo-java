059e5688ebd6a9ce711b820446a0b5b6747da8a8
Adam Thody <athody@heroku.com>
2016-07-12 09:53:14 -0400

    Use less ugly config key

--- src/main/java/com/heroku/kafka/demo/DemoConfiguration.java
+++ src/main/java/com/heroku/kafka/demo/DemoConfiguration.java
@@ -1,5 +1,6 @@
 package com.heroku.kafka.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.loginbox.heroku.config.HerokuConfiguration;
 
 import javax.validation.Valid;
@@ -7,6 +8,7 @@ import javax.validation.Valid;
 public class DemoConfiguration extends HerokuConfiguration {
 
   @Valid
+  @JsonProperty("kafka")
   private final KafkaConfig kafkaConfig = new KafkaConfig();
 
   public KafkaConfig getKafkaConfig() {
