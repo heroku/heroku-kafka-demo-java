6f89dbf57040717a150baac056416cf6b7e9013b
Adam Thody <athody@heroku.com>
2016-07-12 09:50:32 -0400

    Validate KAFKA_TOPIC env var at boot time

--- src/main/java/com/heroku/kafka/demo/DemoConfiguration.java
+++ src/main/java/com/heroku/kafka/demo/DemoConfiguration.java
@@ -2,7 +2,11 @@ package com.heroku.kafka.demo;
 
 import com.loginbox.heroku.config.HerokuConfiguration;
 
+import javax.validation.Valid;
+
 public class DemoConfiguration extends HerokuConfiguration {
+
+  @Valid
   private final KafkaConfig kafkaConfig = new KafkaConfig();
 
   public KafkaConfig getKafkaConfig() {
